073299acd363669ccd35c28991c556fd01da978d
mrmasterix <mrmasterix@gmail.com>
2016-12-28 16:13:44 +0200

    Clear branch

--- dev/grunt/jscs.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  options: {
-    config: '<%= gruntPluginsDir %>' + '/.jscsrc',
-    fix: false, // Autofix code style violations when possible.
-    requireCurlyBraces: [ "if" ],
-  },
-
-  src: '<%= sourceJSMy %>',
-};
--- sitemap_netcraft.php
+++
@@ -1,6 +0,0 @@
-<?php
-$type = 'netcraft';
-$company = 'Net-Craft';
-$link = 'http://net-craft.com/';
-require('inc/sitemap.php');
-?>
